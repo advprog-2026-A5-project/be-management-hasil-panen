c5dba77e3cb35b5d7581f504ae39928fb39857a6
Matthew Wijaya <matthew.wijaya@ui.ac.id>
2026-05-22 23:43:25 +0700

    fix sonar properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,10 +84,13 @@ tasks.jacocoTestReport {
 
 sonar {
     properties {
-        property "sonar.projectKey", System.getenv("SONAR_PROJECT_KEY")
-        property "sonar.organization", System.getenv("SONAR_ORGANIZATION")
-        property "sonar.host.url", "https://sonarcloud.io"
-        property "sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
+        property("sonar.projectKey", System.getenv("SONAR_PROJECT_KEY") ?: "")
+        property("sonar.organization", System.getenv("SONAR_ORGANIZATION") ?: "")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml"
+        )
     }
 }
 
